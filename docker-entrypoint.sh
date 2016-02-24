d49f9730e7024ca44cf057fa72dfe4b57be6fe2b
Sebastian Maier <sebastianmaier@users.noreply.github.com>
2016-02-24 18:16:36 +0100

    change nginx user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ ! -e '/var/www/html/piwik.php' ]; then
 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-	chown -R www-data /var/www/html
+	chown -R nginx /var/www/html
 fi
 
 while /bin/true; do
-  su -s "/bin/bash" -c "/usr/local/bin/php /var/www/html/console core:archive" www-data
+  su -s "/bin/bash" -c "/usr/local/bin/php /var/www/html/console core:archive" nginx
   sleep 3600
 done &
 
