f20026be1d76f0741e07e383af6a731d7f19d2b9
Sebastian Maier <sebastianmaier@users.noreply.github.com>
2016-02-24 19:17:22 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ ! -e '/var/www/html/piwik.php' ]; then
 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
-	chown -R nginx /var/www/html
+	chown -R www-data /var/www/html
 fi
 
 while /bin/true; do
-  su -s "/bin/bash" -c "/usr/local/bin/php /var/www/html/console core:archive" nginx
+  su -s "/bin/bash" -c "/usr/local/bin/php /var/www/html/console core:archive" www-data
   sleep 3600
 done &
 
